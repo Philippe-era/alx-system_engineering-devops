bf3630930065f691e1a0fe39dbc1bc2b54a797c1
= <=>
2023-10-23 04:52:23 -0700

    fixing

--- 0x15-api/2-export_to_JSON.py
+++ 0x15-api/2-export_to_JSON.py
@@ -9,7 +9,7 @@ if __name__ == "__main__":
     url_1 = "https://jsonplaceholder.typicode.com/"
     user_check = requests.get(url_1 + "users/{}".format(user_id)).json()
     username = user_check.get("username")
-    list_1 = requests.get(url_1 + "todos",params={"userId": user_id}).json()
+    list_1 = requests.get(url_1 + "todos", params={"userId": user_id}).json()
 
     with open("{}.json".format(user_id), "w") as jsonfile:
         json.dump({user_id: [{
