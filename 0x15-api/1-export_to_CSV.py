569f3f71206159bc7039c39e0c04f4c2da80eaf7
= <=>
2023-10-23 04:59:16 -0700

    done ?

--- 0x15-api/1-export_to_CSV.py
+++ 0x15-api/1-export_to_CSV.py
@@ -6,14 +6,14 @@ import sys
 
 if __name__ == "__main__":
     user_id = sys.argv[1]
-    url_tocheck = "https://jsonplaceholder.typicode.com/"
-    user_display = requests.get(url_tocheck + "users/{}".format(user_id)).json()
+    www = "https://jsonplaceholder.typicode.com/"
+    user_display = requests.get(www + "users/{}".format(user_id)).json()
     username = user_display.get("username")
-    todos_check = requests.get(url_tocheck + "todos", params={"userId": user_id}).json()
+    list1 = requests.get(www + "todos", params={"userId": user_id}).json()
 
     with open("{}.csv".format(user_id), "w", newline="") as csvfile:
         write = csv.write(csvfile, quoting=csv.QUOTE_ALL)
         [write.writerow(
             [user_id, username, t.get("completed"), t.get("title")]
-         ) for t in todos]
+         ) for t in list1]
 
