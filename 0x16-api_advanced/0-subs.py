abef16c5d2ae8b5a8e2293bb890bb60dbba42923
= <=>
2023-11-07 07:25:02 -0800

    sub.py

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -5,9 +5,9 @@ import requests
 
 
 def number_of_subscribers(subreddit):
-    link = "https://www.reddit.com/r/{}/about.json".format(subreddit)
-    title_link = {'User-Agent': 'Mozilla/5.0'}
-    response = requests.get(link, title_link=title_link, allow_redirects=False)
+    url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
+    headers = {'User-Agent': 'Mozilla/5.0'}
+    response = requests.get(url, headers=headers, allow_redirects=False)
     data = response.json()
     if response.status_code != 200:
         return 0
